4b07907b15f2ef1306b6f4faeba1f2d724a188b5
Marco Terrones <mterrones.ra@gmail.com>
2021-04-09 21:10:09 -0500

    feat:

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
 
 Route::post('/test', function () {
 
+    \Log::info(request()->all());
     return response()->json(['success' => true, 'data' => request()->all()]);
 
 });
